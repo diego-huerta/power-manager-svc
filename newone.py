2b3d816bceb418d832686b86df05a83476d968bc
Diego Huerta <diego.huerta@gemini-is.com>
2021-11-02 20:57:16 -0600

    Added factor

--- newone.py
+++ newone.py
@@ -29,7 +29,7 @@ start = time.monotonic()
 # Read the same channel over and over
 for i in range(SAMPLES):
 #    data[i] = chan.value
-    print(chan.voltage)
+    print(chan.voltage * 3.040588318)
 
 #end = time.monotonic()
 #total_time = end - start
