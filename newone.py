376aed76d338dca5b6e61c9b95c0e383f7ef4d57
Diego Huerta <diego.huerta@gemini-is.com>
2021-11-03 01:30:41 -0600

    Changed from ADS1015 to ADS1115 and cleaned up unused vars

--- newone.py
+++ newone.py
@@ -1,7 +1,7 @@
 import time
 import board
 import busio
-import adafruit_ads1x15.ads1015 as ADS
+import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 
@@ -15,9 +15,8 @@ SAMPLES = 1000
 i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
 
 # Create the ADC object using the I2C bus
-ads = ADS.ADS1015(i2c)
+ads = ADS.ADS1115(i2c)
 
-# Create single-ended input on channel 0
 chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
 # ADC Configuration
@@ -26,8 +25,6 @@ ads.data_rate = RATE
 
 data = [None]*SAMPLES
 
-start = time.monotonic()
-
 # Read the same channel over and over
 for i in range(SAMPLES):
     data[i] = chan.voltage
@@ -35,9 +32,6 @@ for i in range(SAMPLES):
 print('antes' + '\t' + 'despues')
 for sample in data:
     print(str(sample) + '\t' + str(sample * FACTOR))
-
-#end = time.monotonic()
-#total_time = end - start
 
 #print("Time of capture: {}s".format(total_time))
 #print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
