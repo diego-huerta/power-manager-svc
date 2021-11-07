252b369e8bd0f2b531012d14b4f7afe9f89747b8
Diego Huerta (Mysshell) <diego.huerta@gemini-is.com>
2021-11-07 01:37:36 -0600

    Added first draft of CurrentMonitor.py

--- newone.py
+++ newone.py
@@ -1,4 +1,3 @@
-import time
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
@@ -8,7 +7,7 @@ from adafruit_ads1x15.analog_in import AnalogIn
 # Data collection setup
 FACTOR = 42.425519
 RATE = 3300
-SAMPLES = 1000
+SAMPLES = 40
 
 # test
 # Create the I2C bus with a fast frequency
@@ -23,15 +22,21 @@ chan = AnalogIn(ads, ADS.P0, ADS.P1)
 ads.mode = Mode.CONTINUOUS
 ads.data_rate = RATE
 
-data = [None]*SAMPLES
+data = [None] * SAMPLES
 
 # Read the same channel over and over
-for i in range(SAMPLES):
-    data[i] = chan.voltage
+def take_samples(buffer, number_of_samples):
+    for i in range(number_of_samples):
+        buffer[i] = chan.voltage
+
+take_samples()
 
 print('antes' + '\t' + 'despues')
 for sample in data:
     print(str(sample) + '\t' + str(sample * FACTOR))
 
-#print("Time of capture: {}s".format(total_time))
-#print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
+# print("Time of capture: {}s".format(total_time))
+# print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
+
+
+
