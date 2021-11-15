706280cde0b5bd9c0aace8fc1a12b3d88b20241e
Diego Huerta (Mysshell) <diego.huerta@gemini-is.com>
2021-11-15 14:43:05 -0600

    Added __init__.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-from src.monitors.PowerMonitor import PowerMonitor
+#!/bin/python3 -m
+from monitors.PowerMonitor import PowerMonitor
 
 monitor1 = PowerMonitor(frequency=1000000, rate=3300, sample_size=100, factor=42.425519)
 monitor1.start_recording()
