f17685296ec23cacc338e7ce8adf498b4a8ca93f
Diego Huerta <diego.huerta@gemini-is.com>
2021-11-22 01:13:26 +0000

    Fixed dependencies

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #!/bin/python3 -m
-from monitors.PowerMonitor import PowerMonitor
+from src.monitors.PowerMonitor import PowerMonitor
 
-monitor1 = PowerMonitor(frequency=1000000, rate=3300, sample_size=100, factor=42.425519)
+monitor1 = PowerMonitor(frequency=1000000, rate=3300, sample_size=100)
 monitor1.start_recording()
